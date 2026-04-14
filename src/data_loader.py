95f5aae424ba575cf715269380cc7bfcc3658246
dklodowski@student.agh.edu.pl <dklodowski@student.agh.edu.pl>
2026-04-14 16:51:24 +0200

    DataManager.get_daily_return() removed

--- src/data_loader.py
+++ src/data_loader.py
@@ -32,17 +32,17 @@ class DataManager:
     path: pathlib.Path
     raw_data: pd.DataFrame | None
     close_prices: pd.DataFrame | None
-    close_returns: pd.DataFrame | None
     open_prices: pd.DataFrame | None
-    open_returns: pd.DataFrame | None
+    close_returns: pd.DataFrame | None
     """
     def __init__(self, database_path: str | Path):
         self.path = Path(database_path)
+
         self.raw_data: pd.DataFrame | None = None
         self.close_prices: pd.DataFrame | None = None
-        self.close_returns: pd.DataFrame | None = None
         self.open_prices: pd.DataFrame | None = None
-        self.open_returns: pd.DataFrame | None = None
+
+        self.close_returns: pd.DataFrame | None = None
 
     def load_everything(self, selected_files: list[str] | None = None):
         """
@@ -81,54 +81,7 @@ class DataManager:
         self.close_prices = self.raw_data["close"]
         self.open_prices = self.raw_data["open"]
 
-    def get_daily_returns(self, price_type: str = "close") -> pd.DataFrame:
-        """
-        Calculates percentage returns for given price type.
-        param: price_type: str
-        return: pd.DataFrame
-        """
-        price_map = {
-            "close": (self.close_prices, "close_returns"),
-            "open": (self.open_prices, "open_returns"),
-        }
-
-        if price_type not in price_map:
-            raise ValueError(f"Unsupported price_type: {price_type}")
-
-        prices, attr_name = price_map[price_type]
-        returns = getattr(self, attr_name)
-
-        if returns is None:
-            if prices is None:
-                raise ValueError(f"{price_type.capitalize()} prices not loaded")
-
-            returns = prices.pct_change().dropna()
-            setattr(self, attr_name, returns)
-
-        return returns
-
-    def get_total_returns(self, price_type: str = "close") -> pd.DataFrame:
-        price_map = {
-            "close": (self.close_prices, "close_returns"),
-            "open": (self.open_prices, "open_returns"),
-        }
-
-        if price_type not in price_map:
-            raise ValueError(f"Unsupported price_type: {price_type}")
-
-        prices, attr_name = price_map[price_type]
-        overall_returns = getattr(self, attr_name)
-
-        if overall_returns is None:
-            if prices is None:
-                raise ValueError(f"{price_type.capitalize()} prices not loaded")
-
-            first_prices = prices.bfill().iloc[0]
-            overall_returns = (prices - first_prices) / first_prices * 100
-
-            setattr(self, attr_name, overall_returns)
-
-        return overall_returns
+        self.close_returns = self.close_prices.pct_change().dropna()
 
     def comparison_df_prep(self, combinations: list) -> pd.DataFrame:
 
